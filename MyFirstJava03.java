37b526eaaf8f7098c28d9498ca6659a56fd4dcf3
desydwipuspita <118228888+desydwipuspita@users.noreply.github.com>
2024-09-06 08:01:44 +0700

    Add files via upload

---
+++ MyFirstJava03.java
@@ -0,0 +1,17 @@
+public class MyFirstJava03  {
+
+    public static void main(String[]args) {
+
+        System.out.println("--------------------------------------------------------");  
+        System.out.println("-- DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");  
+        //System.out.println("\nsaya adalah Mahasiswa Prodi Sistem Informasi Bisnis");  
+        System.out.println("Nama       : Desy Dwi Puspita");  
+        System.out.println("NIM        : 244107060145");  
+        System.out.println("Kelas      : SIB 1A");  
+        System.out.println("No Absen   : 3");  
+        System.out.println("Prodi      : SISTEM INFORMASI BISNIS");  
+        System.out.println("Alamat     : JL.Simbar Menjangan no.39");  
+        System.out.println("No HP      : 081553257587");  
+        System.out.println("Hobi       :OLAHRAGA");
+    }
+}
