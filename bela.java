da93f8133fa3aac1109d17cba0bb29ea1c5b62ef
nita <primayunitaagustine28@gmail.com>
2024-09-06 15:58:21 +0700

    commit nita

---
+++ bela.java
@@ -0,0 +1,9 @@
+public class bela {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama   : Desy Dwi p");
+        System.out.println("NIM    : 244107060145");
+        System.out.println("Alamat : Simbar");
+        System.out.println("-----------------------------------------------");
+    }
+}
---
+++ biodatanita.java
@@ -0,0 +1,9 @@
+public class biodatanita {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama   : Primayunita Putri Agustine");
+        System.out.println("NIM    : 244107060094");
+        System.out.println("Alamat : Jombang");
+        System.out.println("-----------------------------------------------");
+    }
+}
