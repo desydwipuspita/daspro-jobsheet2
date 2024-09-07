47ca7d9f2a463765841c16c0bd23b5e838078a23
Desy <desypuspita685@gmail.com>
2024-09-07 16:33:45 +0700

    commitdesy

---
+++ biodatadesy.java
@@ -0,0 +1,8 @@
+public class MyFirstJava03  {
+
+    public static void main(String[]args) { 
+        System.out.println("Nama       : Desy ");  
+        System.out.println("NIM        : 244107060145");  
+        System.out.println("Kelas      : SIB 1A");  
+    }
+}
